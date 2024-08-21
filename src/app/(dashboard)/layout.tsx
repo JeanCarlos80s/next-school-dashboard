13a389921cc3443837bf8a8923ba665135994da0
jean carlos <jeancarlos80s@gmail.com>
2024-08-21 10:42:58 -0300

    Menu Design

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,8 @@
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function DashboardLayout({
     children,
   }: Readonly<{
@@ -6,9 +11,18 @@ export default function DashboardLayout({
     return (
         <div className="h-screen flex">
           {/* LEFT */}
-          <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">LEft</div>
+          <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+            <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
+              <Image src="/logo.png" alt="Logo" width={32} height={32} />
+              <span className="hidden lg:block">SchoolLama</span>
+            </Link>
+            <Menu />
+          </div>
           {/* RIF+GHT */}
-          <div className="w-[84%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">RIGHT</div>
+          <div className="w-[84%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+            <Navbar />
+            {children}
+          </div>
         </div>
     );
   }
